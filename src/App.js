ea8cc1d39f375e409d699d1a34e480bbe59f61bd
AmithChandru <amithc279@gmail.com>
2023-04-21 17:29:01 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import Inbox from './components/Inbox';
 import Email from './components/Email';
 import SentEmail from './components/SentEmail';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from './store/AuthReducer';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const token = useSelector(state => state.auth.token);
+
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
+        <NavLink to='/login'>
+          {token && <button className='LogoutButton' onClick={handleLogout}>Logout</button>}
+        </NavLink>
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
